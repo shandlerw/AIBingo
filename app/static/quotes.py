725a297cc8f5c3fc0fd7fc7e976d5af557a6f100
alan-r-armstrong <alan.armstrong@gmail.com>
2021-06-29 11:41:32 -0700

    quotes updated

--- app/static/quotes.py
+++ app/static/quotes.py
@@ -4,5 +4,6 @@ logging.basicConfig(level=logging.INFO)
 
 luke_taunts = ["Just like beggar's canyon back home", "You should've bargained, that's the last mistake you'll ever make.", "You failed, your highness.", "Your overconfidence is your weakness.","You'll Find I'm Full Of Surprises.","The Force Is Strong In My Family.","Your Thoughts Betray You.", "This is not going to go the way you think","I warn you not to underestimate my powers.","Your thoughts betray you","I'm Luke Skywalker, I'm here to rescue you.","You're gravely mistaken"
 ]
+leia_taunts = ["Someone has to save our skins", "Why, you stuck-up, half-witted, scruffy-looking nerf herder!", "Into the garbage chute, flyboy", ""]
 
 logging.info(random.choice(luke_taunts))
