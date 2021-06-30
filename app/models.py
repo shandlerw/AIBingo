61bbc599a344a7367882c506ad6f6d6209942783
Shandler Whitson <shandler@trinityproximity.com>
2021-06-30 16:47:32 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,4 +7,9 @@ from django.db.models.fields import EmailField, IntegerField, related
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
-# Create your models here.
+class character(models.Model):
+    name = models.CharField(max_length=15)
+    affiliation = models.CharField(max_length=15)
+    char_pic = models.CharField (max_length=50)
+    char_desc = models.CharField (max_length=250)
+    char_quotes = models.CharField (max_length= 125)
